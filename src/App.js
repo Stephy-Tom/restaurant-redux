273db2f480991398ce668d00fa4f9e0478d01191
Stephy-Tom <stephymathew610@gmail.com>
2024-01-30 17:44:14 +0530

    restaurant app using redux and thunk

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import { Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import RestView from './pages/RestView';
+
+function App() {
+  return (
+    <div>
+    <Header/>
+    <Routes>
+      <Route path='/' element={<Home/>} />
+      <Route path='/restaurant-view/:id' element={<RestView/>} />
+    </Routes>
+
+    <Footer/>
+   </div>
+  );
+}
+
+export default App;
